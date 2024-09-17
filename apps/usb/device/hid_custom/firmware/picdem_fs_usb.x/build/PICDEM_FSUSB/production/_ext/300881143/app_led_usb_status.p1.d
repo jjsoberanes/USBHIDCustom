0e3d7522290c4397c1445c5ee845aa45741a308a
John <jonathanujsov@gmail.com>
2024-09-17 15:42:33 -0600

    Ejemplo USB HID (USB HID Custom)

---
+++ apps/usb/device/hid_custom/firmware/picdem_fs_usb.x/build/PICDEM_FSUSB/production/_ext/300881143/app_led_usb_status.p1.d
@@ -0,0 +1,14 @@
+build/PICDEM_FSUSB/production/_ext/300881143/app_led_usb_status.p1:  \
+../demo_src/app_led_usb_status.c  \
+system.h  \
+../../../../../../bsp/picdem_fs_usb/buttons.h  \
+../../../../../../bsp/picdem_fs_usb/leds.h  \
+../../../../../../bsp/picdem_fs_usb/adc.h  \
+io_mapping.h  \
+fixed_address_memory.h  \
+../../../../../../framework/usb/inc/usb_device.h  \
+../../../../../../framework/usb/inc/usb_common.h  \
+../demo_src/usb_config.h  \
+../../../../../../framework/usb/inc/usb_ch9.h  \
+../../../../../../framework/usb/inc/usb_hal.h  \
+../../../../../../framework/usb/inc/usb_hal_pic18.h 
---
+++ apps/usb/device/hid_custom/firmware/picdem_fs_usb.x/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# PICDEM_FSUSB configuration
+CND_ARTIFACT_DIR_PICDEM_FSUSB=dist/PICDEM_FSUSB/production
+CND_ARTIFACT_NAME_PICDEM_FSUSB=picdem_fs_usb.x.production.hex
+CND_ARTIFACT_PATH_PICDEM_FSUSB=dist/PICDEM_FSUSB/production/picdem_fs_usb.x.production.hex
